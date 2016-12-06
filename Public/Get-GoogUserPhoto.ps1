9072a9989dbe23879867ab5f0fe887aa25ed5d38
Nate Ferrell <nate@themassteringhouse.com>
2016-12-06 11:09:50 -0800

    added Get-GoogUserPhoto

---
+++ Public/Get-GoogUserPhoto.ps1
@@ -0,0 +1,54 @@
+﻿function Get-GoogUserPhoto {
+    [cmdletbinding()]
+    Param
+    (      
+      [parameter(Mandatory=$true)]
+      [String]
+      $User,
+      [parameter(Mandatory=$false)]
+      [String]
+      $AccessToken,
+      [parameter(Mandatory=$false)]
+      [ValidateNotNullOrEmpty()]
+      [String]
+      $P12KeyPath = $Script:PSGoogle.P12KeyPath,
+      [parameter(Mandatory=$false)]
+      [ValidateNotNullOrEmpty()]
+      [String]
+      $AppEmail = $Script:PSGoogle.AppEmail,
+      [parameter(Mandatory=$false)]
+      [ValidateNotNullOrEmpty()]
+      [String]
+      $AdminEmail = $Script:PSGoogle.AdminEmail
+    )
+if (!$AccessToken)
+    {
+    $AccessToken = Get-GoogToken -P12KeyPath $P12KeyPath -Scopes "https://www.googleapis.com/auth/admin.directory.user" -AppEmail $AppEmail -AdminEmail $AdminEmail
+    }
+$header = @{
+    Authorization="Bearer $AccessToken"
+    }
+$URI = "https://www.googleapis.com/admin/directory/v1/users/$User/photos/thumbnail"
+try
+    {
+    $response = Invoke-RestMethod -Method Get -Uri $URI -Headers $header -ContentType "application/json"
+    }
+catch
+    {
+    try
+        {
+        $result = $_.Exception.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($result)
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $resp = $reader.ReadToEnd()
+        $response = $resp | ConvertFrom-Json | 
+            Select-Object @{N="Error";E={$Error[0]}},@{N="Code";E={$_.error.Code}},@{N="Message";E={$_.error.Message}},@{N="Domain";E={$_.error.errors.domain}},@{N="Reason";E={$_.error.errors.reason}}
+        }
+    catch
+        {
+        $response = $resp
+        }
+    }
+return $response
+}
