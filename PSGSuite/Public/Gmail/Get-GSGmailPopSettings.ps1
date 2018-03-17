f7aaa853a56b9f9ff432d09d80b94333fa1fe8d3
Nate Ferrell <nate@scrthq.com>
2018-03-16 19:56:14 -0500

    !build Get-GSGmailPopSettings and Get-GSGmailImapSettings

---
+++ PSGSuite/Public/Gmail/Get-GSGmailPopSettings.ps1
@@ -0,0 +1,57 @@
+function Get-GSGmailPopSettings {
+    <#
+    .SYNOPSIS
+    Gets POP settings
+    
+    .DESCRIPTION
+    Gets POP settings
+    
+    .PARAMETER User
+    The user to get the POP settings for
+
+    Defaults to the AdminEmail user
+    
+    .EXAMPLE
+    Get-GSGmailPopSettings
+
+    Gets the POP settings for the AdminEmail user
+    #>
+    [cmdletbinding()]
+    Param
+    (
+        [parameter(Mandatory = $false,Position = 0,ValueFromPipelineByPropertyName = $true)]
+        [Alias("PrimaryEmail","UserKey","Mail")]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $User = $Script:PSGSuite.AdminEmail
+    )
+    Begin {
+        if ($User -ceq 'me') {
+            $User = $Script:PSGSuite.AdminEmail
+        }
+        elseif ($User -notlike "*@*.*") {
+            $User = "$($User)@$($Script:PSGSuite.Domain)"
+        }
+        $serviceParams = @{
+            Scope       = 'https://www.googleapis.com/auth/gmail.settings.basic'
+            ServiceType = 'Google.Apis.Gmail.v1.GmailService'
+            User        = $User
+        }
+        $service = New-GoogleService @serviceParams
+    }
+    Process {
+        try {
+            $request = $service.Users.Settings.GetPop($User)
+            Write-Verbose "Getting POP settings for user '$User'"
+            $request.Execute() | Select-Object @{N = 'User';E = {$User}},*
+        }
+        catch {
+            if ($ErrorActionPreference -eq 'Stop') {
+                $PSCmdlet.ThrowTerminatingError($_)
+            }
+            else {
+                Write-Error $_
+            }
+        }
+    }
+}
